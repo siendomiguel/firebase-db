c1223f8be141c976417117d2eb41b16185cd7b36
Miguel Lares <siendomigueldev@gmail.com>
2024-11-04 18:34:29 -0500

    add new credentials

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import admin from 'firebase-admin';
 
-const credentials = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+const credentials = JSON.parse(process.env.FIREBASE_CREDENTIALS ?? process.env.FIREBASE_CREDENTIALS_TEST);
 
 initializeApp({
   credential: admin.credential.cert(credentials),
